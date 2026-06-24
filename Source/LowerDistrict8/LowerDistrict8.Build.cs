084c5e100f80a6b2b406906d1d757b3b5427fdd0
Arkru <35932023+Arkru1204@users.noreply.github.com>
2026-06-24 16:58:23 +0900

    [Chore] 빌드 파일 수정
    * Source 폴더 구조 정리

--- Source/LowerDistrict8/LowerDistrict8.Build.cs
+++ Source/LowerDistrict8/LowerDistrict8.Build.cs
@@ -8,16 +8,33 @@ public class LowerDistrict8 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PublicIncludePaths.AddRange(new string[] {
+            "LowerDistrict8",
+            "LowerDistrict8/Player",
+            "LowerDistrict8/Enemy",
+            "LowerDistrict8/AI",
+            "LowerDistrict8/Interface",
+            "LowerDistrict8/UI",
+            "LowerDistrict8/GameMode",
+            "LowerDistrict8/Animation"
+        });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
