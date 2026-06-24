8e5aec52c70e85df720150b9071831ff1f56e048
Arkru <35932023+Arkru1204@users.noreply.github.com>
2026-06-24 15:11:22 +0900

    [Init]

---
+++ Source/LowerDistrict8/LowerDistrict8.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LowerDistrict8.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LowerDistrict8, "LowerDistrict8" );
